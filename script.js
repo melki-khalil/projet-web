096ea40f74f0806b47b7299c1c10db23fd6d3027
melki-khalil <khalil132676@gmail.com>
2024-12-21 16:02:12 +0100

    modifications

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
-function changeLanguage(lang) {
-    const currentUrl = window.location.href.split('?')[0];
-    window.location.href = `${currentUrl}?lang=${lang}`;
+  function showToast() {
+
+    var toastElement = document.getElementById('myToast');
+    
+    var toast = new bootstrap.Toast(toastElement);
+    
+    // Show the toast
+    toast.show();
   }
+
+  //enable tooltips
+    document.addEventListener('DOMContentLoaded', function () {
+        const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    });
+
